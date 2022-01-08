98d12991f8142f634dc6c883d0caf86dbf0c9f1c
Mitch Falvo <mitchellfalvo@gmail.com>
2022-01-08 18:33:32 -0500

    Modal issues

--- scripts.js
+++ scripts.js
@@ -1,22 +1,10 @@
-var myModal = document.getElementById("myModal");
-var myInput = document.getElementById("myInput");
 
-myModal.addEventListener("shown.bs.modal", function () {
-  myInput.focus();
-});
+    $(window).on('load', function() {
+      $('#myModal').modal('show');
+  });
 
-// ? https://www.codeply.com/p/0CWffz76Q9
-let items = document.querySelectorAll(".carousel .carousel-item");
-items.forEach((el) => {
-  const minPerSlide = 4;
-  let next = el.nextElementSibling;
-  for (var i = 1; i < minPerSlide; i++) {
-    if (!next) {
-      // wrap carousel by using first child
-      next = items[0];
-    }
-    let cloneChild = next.cloneNode(true);
-    el.appendChild(cloneChild.children[0]);
-    next = next.nextElementSibling;
-  }
-});
+
+
+
+
+
