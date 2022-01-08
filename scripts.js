cf734b087e24827e4f746d9502177316f54f149f
Mitch Falvo <mitchellfalvo@gmail.com>
2022-01-08 13:35:48 -0500

    formatting fixes

--- scripts.js
+++ scripts.js
@@ -4,3 +4,19 @@ var myInput = document.getElementById("myInput");
 myModal.addEventListener("shown.bs.modal", function () {
   myInput.focus();
 });
+
+// ? https://www.codeply.com/p/0CWffz76Q9
+let items = document.querySelectorAll(".carousel .carousel-item");
+items.forEach((el) => {
+  const minPerSlide = 4;
+  let next = el.nextElementSibling;
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // wrap carousel by using first child
+      next = items[0];
+    }
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
