ada84e3b759e6daca492d7d45ebcf005934b9015
Mitch Falvo <mitchellfalvo@gmail.com>
2022-01-08 20:03:58 -0500

    Just in case commit

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
+$(window).on('load', function() {
+   $('#myModal').modal('show');
+})
 
-    $(window).on('load', function() {
-      $('#myModal').modal('show');
-  });
+// * FIXES AUTOFOCUS ISSUE
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
 
-
-
-
-
-
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
