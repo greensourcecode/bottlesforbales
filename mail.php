7320877a35fbcfc70c9dd0cbf16e8982f276ef29
Mitch Falvo <mitchellfalvo@gmail.com>
2022-03-06 15:08:01 -0500

    email fix

---
+++ mail.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * @CODY-BY-ARMAN
+ * V1.0
+ */
+
+// The Mail will go to Here #######################################
+$mail_to= 'mitchellfalvo@gmail.com';
+
+// The Email will go from #######################################
+$mail_from = "mitch@bottlesforbales.org";
+
+// After Submiting The Email Where Need to go back...?
+$go_back = "https://www.google.com/";
+
+
+if(isset($_POST['send_to_email'])){
+
+    $number_of_bag = $_POST['number_of_bag'];
+    $select_country = $_POST['select_country'];
+    $name = $_POST['name'];
+    $streetAddress = $_POST['streetAddress'];
+    $phoneNumber = $_POST['phoneNumber'];
+    $bagLocation = $_POST['bagLocation'];
+
+    $subject = 'Form Submition';
+    $message = '<h3>Submited Form Information------------></h3>
+        <b>Number of Bags:</b> '. $number_of_bag . '<br>
+        <b>County:</b> '. $select_country . '<br>
+        <b>Name:</b> '. $name . '<br>
+        <b>Street Address:</b> '. $streetAddress . '<br>
+        <b>Phone Number:</b> '. $phoneNumber . '<br>
+        <b>Bag Location:</b> '. $bagLocation . '<br>
+    ';
+
+    $headers = 'From: '. $mail_from . "\r\n" .
+    'Reply-To: '. $mail_from . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+
+    if( mail($mail_to, $subject, $message, $headers) ){
+        $submited = 'true';
+    }else{
+        $submited = 'false';
+    }
+
+    echo $message;
+}
+header( "Location: $go_back?mail-submited=$submited" );
+
+?>
